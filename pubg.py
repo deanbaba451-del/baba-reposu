63bf678a35d1ebb0659f7aa6913f74768159af63
deanbaba451-del <deanbaba451@gmail.com>
2026-01-08 01:13:18 +0300

    Update pubg.py

--- pubg.py
+++ pubg.py
@@ -1,34 +1,42 @@
-import pyfiglet
-print(pyfiglet.figlet_format("Pubg Tool"))
-import random
-import requests
 import webbrowser
-import os
+import requests
+import random
+print("PUBG HESAP        RAİZEN STORE SUNAR")
 import json
+import os
 import requests, time
+import webbrowser
+webbrowser.open('https://t.me/+Q34zmJI_FZozOGNi')
 
-webbrowser.open("https://t.me/Mrzpyx")
 
-tok = input("8110267443:AAHNAgx0Yleg6JKLXomoTuhB_zEte-g-8HI: ")
-id = input("6534222591: ")
-accounts_count = int(input("Hesap Sayısı: "))
+tok = input("8110267443:AAHNAgx0Yleg6JKLXomoTuhB_zEte-g-8HI:")
+ID = input("6534222591: ")
+accounts_count = int(input(" Hesap sayısı: "))
+
 
 for _ in range(accounts_count):
-    characters = "123456789QWERTYUİOPASDFGHJKLZXCVBNM"
+    characters = "1234567890QWERTYUIOPASDFGHJKLXCVBNM"
+    
+    
     us = ''.join(random.choice(characters) for _ in range(7))
     username = "GE" + us
     password = "BF" + us
     us4 = ''.join(random.choice(characters) for _ in range(8))
-      
-    tlg_message = f'''https://api.telegram.org/bot{tok}/sendMessage?chat_id={id}&text=ᯓ ʟᴏɢɪɴ ➥ Facebook\n\n 
-\n 🎖 😶‍🌫 𝙴 posta ➪ {username}@gmail.com
-\n 🎖 😶‍🌫 Şifre ➪ +{us4}
-\n 🎖🀽򝠍🌫 Ülke ➪ Türkiye
-\n\nᯓ BY 🀠: @mrzpyx 𓏺< </> - @KorkumYokki'''   
+    
+    
+    tlg_message = f'''https://api.telegram.org/bot{tok}/sendMessage?chat_id={ID}&text=ᯓ ʟᴏɢɪɴ ➥ Facebook\n\n 
+\n ✓ ➥ 𝙴 posta ➪ {username}@gmail.com
+\n ✓ ➥ Şifre ➪ +{us4}
+\n ✓ ➥ Ülke ➪ Türkiye
+\n ✓ ➥ Kod ➪ +90
+\n ✓ ➥ Zaman ➪ 2022-10-22
+\n\nᯓ BY : 𓏺 Zeynalovs </> - @Zeynalovs5'''
+    
 
     response = requests.post(tlg_message)
     
+ 
     if response.status_code == 200:
-        print(f"[✓] Hesap gönderme başarılı: {username}")
+        print(f"[✓]Hesab gönderme başarılı: {username}")
     else:
-        print(f"[✗] Gönderme hatası: {username}"
+        print(f"[✗] Gönderme hatası: {username}")
